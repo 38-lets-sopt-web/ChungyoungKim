018280918cc9e6b8f3bd2ed8913c1d710ed7c61f
chungyo <kimchungy98@gachon.ac.kr>
2026-05-03 23:20:07 +0900

    ✨ feat: 4주차 실습 구현

---
+++ Week4/Practice/src/router/Router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import UserDetail from "../pages/UserDetail";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: Home,
+    },
+    {
+        path: "/user/:id",
+        Component: UserDetail,
+    },
+]);
+
+export default router;
