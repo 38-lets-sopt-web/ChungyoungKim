d63a13665c0c30224fec61aae5f24d3e5f652934
chungyo <kimchungy98@gachon.ac.kr>
2026-05-05 13:57:30 +0900

    ♻️ refactor: 마이페이지 구조 수정

---
+++ Week4/Homework/src/features/mypage/layouts/MypageLayout.tsx
@@ -0,0 +1,22 @@
+import type { CSSObject } from '@emotion/react'
+import { Outlet } from 'react-router'
+
+import { MypageHeader } from '@/features/mypage/components/MypageHeader'
+import { useMypageUser } from '@/features/mypage/hooks/useMypageUser'
+import { theme } from '@/shared/styles/theme'
+
+export function MypageLayout() {
+  const mypageUser = useMypageUser()
+
+  return (
+    <div css={layoutStyle}>
+      <MypageHeader mypageUser={mypageUser} />
+      <Outlet />
+    </div>
+  )
+}
+
+const layoutStyle: CSSObject = {
+  minHeight: '100vh',
+  background: theme.colors.background,
+}
