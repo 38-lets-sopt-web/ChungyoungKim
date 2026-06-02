7ff09792dd066dca2d40a9bb1f9cf8c4410db557
chungyo <kimchungy98@gachon.ac.kr>
2026-06-02 17:52:38 +0900

    ✨ feat: 별점 필터 옵션 추가

--- Week6/Homework/src/features/movies/constants.ts
+++ Week6/Homework/src/features/movies/constants.ts
@@ -5,8 +5,44 @@ export const MOVIE_ROUTES = {
 
 export const MOVIE_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500'
 
-export const RATING_FILTER = {
-  min: 0,
-  max: 10,
-  step: 0.5,
-} as const
+export const RATING_FILTER_OPTIONS = [
+  {
+    id: 'all',
+    label: '전체 별점',
+    params: {},
+  },
+  {
+    id: 'over-9',
+    label: '9점 이상',
+    params: {
+      voteAverageGte: 9,
+      voteAverageLte: 10,
+    },
+  },
+  {
+    id: 'over-8',
+    label: '8점 이상',
+    params: {
+      voteAverageGte: 8,
+      voteAverageLte: 10,
+    },
+  },
+  {
+    id: 'over-7',
+    label: '7점 이상',
+    params: {
+      voteAverageGte: 7,
+      voteAverageLte: 10,
+    },
+  },
+  {
+    id: 'over-6',
+    label: '6점 이상',
+    params: {
+      voteAverageGte: 6,
+      voteAverageLte: 10,
+    },
+  },
+] as const
+
+export type RatingFilterId = (typeof RATING_FILTER_OPTIONS)[number]['id']
