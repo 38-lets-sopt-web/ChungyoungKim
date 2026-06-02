20e0c3d88b0d2862afea21fc63a4724e4d477357
chungyo <kimchungy98@gachon.ac.kr>
2026-06-02 22:22:24 +0900

    ✨ feat: rating 상수 추가

--- Week6/Homework/src/features/movies/constants.ts
+++ Week6/Homework/src/features/movies/constants.ts
@@ -1,9 +1,20 @@
 export const MOVIE_ROUTES = {
   list: '/',
-  detail: (movieId: string | number) => `/movies/${movieId}`,
+  detail: (movieId: string | number) => `/${movieId}`,
 } as const
 
-export const MOVIE_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500'
+export const MOVIE_POSTER_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500'
+
+export const MOVIE_BACKDROP_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w1280'
+
+export const MOVIE_RATING_STORAGE_KEY = 'week6-movie-ratings'
+
+export const USER_RATING = {
+  min: 0.5,
+  max: 10,
+  step: 0.5,
+  rangeLabel: '0.5 ~ 10.0',
+} as const
 
 export const RATING_FILTER_OPTIONS = [
   {
